1112c75acbcee02c23f1263f18e514c7c4aed9e4
mszumlewicz <83343590+mszumlewicz@users.noreply.github.com>
2021-06-01 19:02:51 +0200

    Update chodzenie.py

--- columbus/chodzenie.py
+++ columbus/chodzenie.py
@@ -1,6 +1,52 @@
-import pygame
-pygame.init()
+import pygame, sys
 
-screen = pygame.display.set_mode([500, 500])
+def moving_background():
+    screen.blit(bg_surface,(bg_x_pos, 0))
+    screen.blit(bg_surface,(bg_x_pos + 0, 0))
+
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
+pygame.init()
+pygame.display.set_caption("Paprotnica")
+screen = pygame.display.set_mode([580, 290])
+clock = pygame.time.Clock()
+
+x= 50
+y= 210
+width = 20
+height = 20
+vel = 5
+
+bg_surface = pygame.image.load('gra/bg.jpg').convert()
+bg_x_pos = 0
+
+playerImg = pygame.image.load('gra/girl.png')
+playerX = 30
+playerY = 170
+
+while True:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    bg_x_pos -= 1
+    moving_background()
+    if bg_x_pos <= -580:
+        bg_x_pos = 0
+
+    player()
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT] and x>0:
+        x -= vel
+    if keys[pygame.K_RIGHT] and x<500-width:
+        x += vel
+
+
+    pygame.display.update()
+    clock.tick(50)
 
 pygame.quit()
