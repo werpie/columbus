a8484ec29836e630b7a0d3e1ea72d8c3e6535ac0
werpie <83338547+werpie@users.noreply.github.com>
2021-06-01 20:39:54 +0200

    Update kolko_i_krzyzyk.py

--- columbus/kolko_i_krzyzyk.py
+++ columbus/kolko_i_krzyzyk.py
@@ -14,9 +14,46 @@ class tic_tac_toe():
                 if self.board[0,0] == 0:
                     self.board[0,0] = 1
                     return
-            elif place == "q":
-                if self.board[0,0] == 0:
-                    self.board[0,0] = 1
+            elif place == "w":
+                if self.board[0,1] == 0:
+                    self.board[0,1] = 1
+                    return
+                    
+            elif place == "e":
+                if self.board[0,2] == 0:
+                    self.board[0,2] = 1
+                    return
+
+            elif place == "a":
+                if self.board[1,0] == 0:
+                    self.board[1,0] = 1
+                    return
+
+            elif place == "s":
+                if self.board[1,1] == 0:
+                    self.board[1,1] = 1
+                    return
+
+            elif place == "d":
+                if self.board[1,2] == 0:
+                    self.board[1,2] = 1
+                    return
+
+            elif place == "z":
+                if self.board[2,0] == 0:
+                    self.board[2,0] = 1
+                    return
+
+            elif place == "x":
+                if self.board[2,1] == 0:
+                    self.board[2,1] = 1
+                    return
+
+            elif place == "c":
+                if self.board[2,2] == 0:
+                    self.board[2,2] = 1
+                    return
+                
 
     def show_board(self):
         print(self.board)
