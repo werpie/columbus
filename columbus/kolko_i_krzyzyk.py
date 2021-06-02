4a6a6d5178e3a2f57dfbfb44d2e10efd5cd8b896
SeriousName123 <68062531+SeriousName123@users.noreply.github.com>
2021-06-02 15:54:43 +0200

    Update kolko_i_krzyzyk.py

--- columbus/kolko_i_krzyzyk.py
+++ columbus/kolko_i_krzyzyk.py
@@ -2,58 +2,80 @@ import pygame
 import random
 import numpy as np
 
+
 class tic_tac_toe():
 
     def __init__(self):
-        self.board = np.zeros((3,3))
+        self.board = np.zeros((3, 3))
+        self.possible_moves = ["q","w","e","a","s","d","z","x","c"]
+
+    def filling_board(self,place):
+        if place == "q":
+            if self.board[0, 0] == 0:
+                self.board[0, 0] = 1
+                self.possible_moves.remove("q")
+                return True
+        elif place == "w":
+            if self.board[0, 1] == 0:
+                self.board[0, 1] = 1
+                self.possible_moves.remove("w")
+                return True
+
+        elif place == "e":
+            if self.board[0, 2] == 0:
+                self.board[0, 2] = 1
+                self.possible_moves.remove("e")
+                return True
+
+        elif place == "a":
+            if self.board[1, 0] == 0:
+                self.board[1, 0] = 1
+                self.possible_moves.remove("a")
+                return True
+
+        elif place == "s":
+            if self.board[1, 1] == 0:
+                self.board[1, 1] = 1
+                self.possible_moves.remove("s")
+                return True
+
+        elif place == "d":
+            if self.board[1, 2] == 0:
+                self.board[1, 2] = 1
+                self.possible_moves.remove("d")
+                return True
+
+        elif place == "z":
+            if self.board[2, 0] == 0:
+                self.board[2, 0] = 1
+                self.possible_moves.remove("z")
+                return True
+
+        elif place == "x":
+            if self.board[2, 1] == 0:
+                self.board[2, 1] = 1
+                self.possible_moves.remove("x")
+                return True
+
+        elif place == "c":
+            if self.board[2, 2] == 0:
+                self.board[2, 2] = 1
+                self.possible_moves.remove("c")
+                return True
+        else:
+            return False
+
+    def computer_move(self):
+        move = random.choice(self.possible_moves)
+        self.filling_board(move)
+        print(move)
 
     def player_move(self):
         while True:
             place = input("q,w,e\na,s,d\nz,x,c")
-            if place == "q":
-                if self.board[0,0] == 0:
-                    self.board[0,0] = 1
-                    return
-            elif place == "w":
-                if self.board[0,1] == 0:
-                    self.board[0,1] = 1
-                    return
-                    
-            elif place == "e":
-                if self.board[0,2] == 0:
-                    self.board[0,2] = 1
-                    return
+            if self.filling_board(place) == True:
+                return
 
-            elif place == "a":
-                if self.board[1,0] == 0:
-                    self.board[1,0] = 1
-                    return
-
-            elif place == "s":
-                if self.board[1,1] == 0:
-                    self.board[1,1] = 1
-                    return
-
-            elif place == "d":
-                if self.board[1,2] == 0:
-                    self.board[1,2] = 1
-                    return
-
-            elif place == "z":
-                if self.board[2,0] == 0:
-                    self.board[2,0] = 1
-                    return
-
-            elif place == "x":
-                if self.board[2,1] == 0:
-                    self.board[2,1] = 1
-                    return
-
-            elif place == "c":
-                if self.board[2,2] == 0:
-                    self.board[2,2] = 1
-                    return
-                
 
     def show_board(self):
         print(self.board)
@@ -61,5 +83,10 @@ class tic_tac_toe():
 
 def do():
     x = tic_tac_toe()
-    x.show_board()
-    x.player_move()
+    while True:
+        x.show_board()
+        x.player_move()
+        x.computer_move()
+        print(x.possible_moves)
+        if input() == 1:
+            return
