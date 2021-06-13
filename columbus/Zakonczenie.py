526cd23e3ddda926936966713da42954232241c5
werpie <83338547+werpie@users.noreply.github.com>
2021-06-13 23:22:27 +0200

    Update Zakonczenie.py

--- columbus/Zakonczenie.py
+++ columbus/Zakonczenie.py
@@ -5,15 +5,15 @@ class Zakonczenie(object):
     def __init__(self):
         cut_scene("Rok póżniej")
         printing(['''
-        Po zerwaniu kwiatu paproci osiadłeś/osiadłaś w wielkim pałacu,
+        Po zerwaniu kwiatu paproci osiadłaś w wielkim pałacu,
         w którym wszystkie meble pokryte są złotem,
         nigdy nie brakuje jedzenia, które serwowane jest
         w postaci najbardziej znakomitych potraw, usługuje Ci
         kilkadziesiąt służących, a o czym pomyślisz, to staje się prawdą.
             '''])
         printing(['''
-        Jesteś jednak bardzo samotny/samotna,
-        nie mogłeś/mogłaś podzielić się niczym ze swoimi przyjaciółmi
+        Jesteś jednak bardzo samotna,
+        nie mogłaś podzielić się niczym ze swoimi przyjaciółmi
         i rodziną. Pewnego dnia postanawiasz wyjść z pałacu
         i przejść się po pobliskiej wsi. Panuje w niej straszna bieda.
         W pewnym momencie dostrzegasz 
@@ -26,15 +26,15 @@ class Zakonczenie(object):
         wybor_konca = int(input("Wybierz 1: Pomagasz dziewczynce\nWybierz 2: Odchodzisz"))
         if wybor_konca == 1:
             printing(['''
-        Zdecydowałeś/zdecydowałaś się pomóc biednej dziewczynce.
+        Zdecydowałaś się pomóc biednej dziewczynce.
         W mgnieniu oka Twój imponujący pałac górujący nad wsią
-        znika, znikają twoje bogato obszyte ubrania i znajdujesz się sam/a
+        znika, znikają twoje bogato obszyte ubrania i znajdujesz się sama
         na środku obcej wsi. Postanawiasz wrócić do swojej rodzinnej
         wsi. Boisz się, ale jednocześnie jest Ci jakoś lżej na sercu.
                 '''])
         else:
                     printing(['''
-        Zdecydowałeś/zdecydowałaś się odejść. Wracasz do swojego pałacu,
+        Zdecydowałaś się odejść. Wracasz do swojego pałacu,
         w którym to wszystkie meble pokryte są złotem,
         nigdy nie brakuje jedzenia i usługuje Ci kilkadziesiąt służących.
         Zasiadasz do stołu i zaczynasz zajadać się podanymi potrawami.
