397b6d8aa2ebc928836fdf5f2f45557ad44c9ceb
mszumlewicz <83343590+mszumlewicz@users.noreply.github.com>
2021-06-13 14:38:34 +0200

    Update księżyc+zakończenie.py

--- columbus/księżyc+zakończenie.py
+++ columbus/księżyc+zakończenie.py
@@ -39,27 +39,26 @@ jak się mają?'''])
     print("Zerwij paproć - 2")
     wybor_konca = int(input())
     if wybor_konca == 1:
-        printing(['''
-            Zdecydowałeś/zdecydowałaś się odejść bez kwiatu paproci. Czy była to dobra decyzja?
-            Trudno powiedzieć. Pamiętaj, że zawsze możesz spróbować go zdobyć przy następnej
-            koniunkcji planet.'''])
+        printing(['''Zdecydowałeś/zdecydowałaś się odejść bez kwiatu paproci. Czy była to dobra decyzja?
+Trudno powiedzieć. Pamiętaj, że zawsze możesz spróbować go zdobyć przy następnej
+koniunkcji planet.'''])
         pygame.display.quit()
     else:
         cut_scene(['''Rok później'''])
         printing(['''Po zerwaniu kwiatu paproci osiadłeś/osiadłaś w wielkim
-        pałacu, w którym wszystkie meble pokryte są złotem,
-        nigdy nie brakuje jedzenia, które serwowane jest
-        w postaci najbardziej znakomitych potraw,
-        usługuje Ci kilkadziesiąt służących, a o czym pomyślisz,
-        to staje się prawdą.'''])
+pałacu, w którym wszystkie meble pokryte są złotem,
+nigdy nie brakuje jedzenia, które serwowane jest
+w postaci najbardziej znakomitych potraw,
+usługuje Ci kilkadziesiąt służących, a o czym pomyślisz,
+to staje się prawdą.'''])
         printing(['''Jesteś jednak bardzo samotny/samotna,
-        nie mogłeś/mogłaś podzielić się niczym ze swoimi
-        przyjaciółmi i rodziną. Pewnego dnia postanawiasz wyjść
-        z pałacu i przejść się po pobliskiej wsi. Panuje w niej
-        straszna bieda. W pewnym momencie dostrzegasz kilkuletnią
-        dziewczynkę leżącą na drodze, drżącą z zimna i wychudzoną do kości.
-        Wahasz się, bo bardzo chcesz jej pomóc, ale wiesz że wtedy,
-        straciłbyś/straciłabyś całe swoje bogactwo.'''])
+nie mogłeś/mogłaś podzielić się niczym ze swoimi
+przyjaciółmi i rodziną. Pewnego dnia postanawiasz wyjść
+z pałacu i przejść się po pobliskiej wsi. Panuje w niej
+straszna bieda. W pewnym momencie dostrzegasz kilkuletnią
+dziewczynkę leżącą na drodze, drżącą z zimna i wychudzoną do kości.
+Wahasz się, bo bardzo chcesz jej pomóc, ale wiesz że wtedy,
+straciłbyś/straciłabyś całe swoje bogactwo.'''])
         pygame.display.quit()
         print("Co robisz?")
         print("Pomagasz dziewczynce - 1")
@@ -67,19 +66,19 @@ jak się mają?'''])
         wybór_1 = int(input())
         if wybór_1 == 1:
             printing(['''Zdecydowałeś/zdecydowałaś się pomóc biednej dziewczynce.
-        W mgnieniu oka Twój imponujący pałac górujący nad wsią znika,
-        znikają twoje bogato obszyte ubrania
-        i znajdujesz się sam/a na środku obcej wsi.
-        Postanawiasz wrócić do swojej rodzinnej wsi.
-        Boisz się, ale jednocześnie jest Ci jakoś lżej na sercu.'''])
+W mgnieniu oka Twój imponujący pałac górujący nad wsią znika,
+znikają twoje bogato obszyte ubrania
+i znajdujesz się sam/a na środku obcej wsi.
+Postanawiasz wrócić do swojej rodzinnej wsi.
+Boisz się, ale jednocześnie jest Ci jakoś lżej na sercu.'''])
         else:
             printing(['''Zdecydowałeś/zdecydowałaś się odejść.
-        Wracasz do swojego pałacu, w którym to wszystkie meble
-        pokryte są złotem, nigdy nie brakuje jedzenia
-        i usługuje Ci kilkadziesiąt służących.
-        Zasiadasz do stołu i zaczynasz zajadać się podanymi potrawami.
-        Wszystko wydaje się w porządku, ale czujesz się bardzo samotnie
-        i tak jakbyś tak naprawdę nic nie miał/a.'''])
+Wracasz do swojego pałacu, w którym to wszystkie meble
+pokryte są złotem, nigdy nie brakuje jedzenia
+i usługuje Ci kilkadziesiąt służących.
+Zasiadasz do stołu i zaczynasz zajadać się podanymi potrawami.
+Wszystko wydaje się w porządku, ale czujesz się bardzo samotnie
+i tak jakbyś tak naprawdę nic nie miał/a.'''])
         printing(['''Koniec.'''])
 
 
