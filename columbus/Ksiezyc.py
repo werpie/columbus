596d005bcdac260b4ec7cdd8a5eb306174ff94f8
mszumlewicz <83343590+mszumlewicz@users.noreply.github.com>
2021-06-13 11:18:51 +0200

    Update Ksiezyc.py

--- columbus/Ksiezyc.py
+++ columbus/Ksiezyc.py
@@ -1,6 +1,11 @@
 import pygame
 from kolko_i_krzyzyk import tic_tac_toe
-import Zakonczenie
+import zakończenie
+from functions import *
+
+printing(lista)
+throwing_at_window(text,colour)
+cut_scene(tekst)
 
 def init_display():
     global screen,background
@@ -11,25 +16,35 @@ def init_display():
 class Ksiezyc(object):
     def __init__(self):
         #init_display()
-        print("Wiesz już, że kwiat paproci znajduje się na Księżycu. Udaj się tam.")
-        print("Witaj na Księżycu! To tutaj odnajdziesz mistyczny kwiat paproci, który ma Ci zapewnić wieczne szczęście. ")
+        printing(['''Wiesz już, że kwiat paproci znajduje się na Księżycu. 
+Udaj się tam.'''])
+        cut_scene("Lecimy na Księżyc")
+        printing(['''Witaj na Księżycu! 
+To tutaj odnajdziesz mistyczny kwiat paproci,
+który ma Ci zapewnić wieczne szczęście.'''])
+        printing(['''Widzisz przed sobą miejsce, w którym znajduje się kwiat paproci.
+Jednakże strzeże go kosmiczny Tygrys.
+Aby go stamtąd przegonić, musisz
+wygrać z nim w kółko i krzyżyk.'''])
+        pygame.display.quit()
         tic_tac_toe()
-        print("""\
-            Widzisz przed sobą kwiat paproci. Może on zagwarantować Ci wieczne szczęście, 
-            spełnić Twoje wszelkie marzenia i dać Ci wszystko czego potrzebujesz. Pamiętaj 
-            jednak, że żadną z tych rzeczy nie możesz się podzielić. Czy zerwiesz paproć, 
-            czy po tej męczącej i niebezpiecznej podróży, wolisz zostawić sprawy takimi jak 
-            się mają?
-            """)
+        printing(['''
+            Widzisz przed sobą kwiat paproci. Może on zagwarantować
+Ci wieczne szczęście, spełnić Twoje wszelkie marzenia 
+i dać Ci wszystko czego potrzebujesz. 
+Pamiętaj jednak, że żadną z tych rzeczy 
+nie możesz się podzielić. 
+Czy zerwiesz paproć, czy po tej męczącej 
+i niebezpiecznej podróży, wolisz zostawić sprawy takimi 
+jak się mają?'''])
         wybor_konca = int(input("Wybierz 1: Odejdź bez paprotki\nWybierz 2: Zerwij paproć"))
         if wybor_konca == 1:
-            print("""\
-                Zdecydowałeś/zdecydowałaś się odejść bez kwiatu paproci. Czy była to dobra decyzja?    
-                Trudno powiedzieć. Pamiętaj, że zawsze możesz spróbować go zdobyć przy następnej 
-                koniunkcji planet.""")
+            printing(['''Zdecydowałeś/zdecydowałaś się odejść bez kwiatu paproci. 
+Czy była to dobra decyzja? Trudno powiedzieć. 
+Pamiętaj, że zawsze możesz spróbować go zdobyć 
+przy następnej koniunkcji planet.'''])
         else:
-            Zakonczenie()
-
+            zakończenie()
 
 Ksiezyc()
 
