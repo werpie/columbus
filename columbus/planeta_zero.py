c64b2021a9abda462e1a1d6b3adf50ee1b2e59f3
mszumlewicz <83343590+mszumlewicz@users.noreply.github.com>
2021-06-13 14:40:48 +0200

    Update planeta_zero.py

--- columbus/planeta_zero.py
+++ columbus/planeta_zero.py
@@ -4,16 +4,14 @@ import time
 import os
 from functions import *
 
-printing(['''
-Od wieków wszystkim wiadomo, że w jedyną taką noc w roku,
+printing(['''Od wieków wszystkim wiadomo, że w jedyną taką noc w roku,
 kiedy to Księżyc, Ziemia, Jowisz i Saturn w koniunkcji staną,
 kwitnie paproć, a kto jej kwiatuszek znajdzie,
 urwie i schowa, ten wielkie na ziemi szczęście mieć będzie.
 Noc jednak ta jest bardzo krótka, a kwiat ten niezwykle ciężko znaleźć.
 Jest bardzo niepozorny, a droga do niego pełna jest zagadek
 i niebezpieczeństw.''',
-'''
-Witaj w grze Kosmiczny Kwiat Paproci.
+'''Witaj w grze Kosmiczny Kwiat Paproci.
 Wcielisz się w rolę Paprociary,
 a Twoim zadaniem będzie podróż przez kosmiczne odmęty galaktyki
 w poszukiwaniu mistycznego kwiatu paproci.
@@ -23,8 +21,7 @@ które masz tylko jedno!
 Podejmuj rozważne decyzje i nie daj się zwieść!'''])
 
 while True:
-    printing(['''
-Aby wyruszyć w podróż, w poszukiwaniu kwiatu paproci
+    printing(['''Aby wyruszyć w podróż, w poszukiwaniu kwiatu paproci
 musisz użyć swojego statku kosmicznego,
 który jednak wydaje się być uszkodzony.
 Zdecyduj, której części chcesz przyjrzeć się bliżej.
@@ -34,8 +31,7 @@ Zdecyduj, której części chcesz przyjrzeć się bliżej.
     print("Silnik - wciśnij 2")
     wybór_1 = int(input())
     if wybór_1 == 1:
-        printing(['''
-Zbiornik paliwa wydaje się być uszkodzony.
+        printing(['''Zbiornik paliwa wydaje się być uszkodzony.
 Zdecyduj, która opcja naprawcza będzie najkorzystniejsza.
         '''])
         pygame.display.quit()
@@ -43,12 +39,10 @@ Zdecyduj, która opcja naprawcza będzie najkorzystniejsza.
         print("Wymiana całego zbiornika na nowy - wciśnij 2")
         wybór_2 = int(input())
         if wybór_2 == 1:
-            printing(['''
-Załatanie zbiornika było dobrym wyborem.
+            printing(['''Załatanie zbiornika było dobrym wyborem.
 Uszkodzenie nie było na tyle poważne, aby wymieniać cały zbiornik.'''])
         else:
-            printing(['''
-Wymiana całego zbiornika na nowy, była bezsensownym wydatkiem.
+            printing(['''Wymiana całego zbiornika na nowy, była bezsensownym wydatkiem.
 Załatanie zbiornika w zupełności by wystarczyło.
 No cóż, masz teraz mniej monet, ale przynajmniej nowy zbiornik.'''])
         printing(['''Jednak nadal coś wydaje się być nie
@@ -58,8 +52,7 @@ tak z silnikiem, czy chcesz się temu przyjrzeć?'''])
         print("Nie - wciśnij 2")
         wybór_3 = int(input())
         if wybór_3 == 1:
-            printing(['''
-Coś wydaje się być nie w porządku,
+            printing(['''Coś wydaje się być nie w porządku,
 jednakże nie za bardzo znasz się na budowie silnika statku kosmicznego.
 Czy chcesz poprosić o pomoc mechanika?'''])
             pygame.display.quit()
@@ -67,13 +60,11 @@ Czy chcesz poprosić o pomoc mechanika?'''])
             print("Nie - wciśnij 2")
             wybór_4 = int(input())
             if wybór_4 == 1:
-                printing(['''
-Mechanik sprawdził i naprawił twój statek.
+                printing(['''Mechanik sprawdził i naprawił twój statek.
 Wyniosło Cię to dużo monet, ale dzięki temu jesteś pewien/pewna,
 że dolecimy do każdej planety bezpiecznie.'''])
             else:
-                printing(['''
-Jakimś cudem samemu udało Ci się naprawić usterkę.
+                printing(['''Jakimś cudem samemu udało Ci się naprawić usterkę.
 Dzięki temu zaoszczędziłeś/zaoszczędziłaś trochę monet,
 ale nie wiadomo, czy usterka jest naprawiona poprawnie.
 Miejmy nadzieję, że dolecisz na miejsce.'''])
@@ -84,15 +75,13 @@ Miejmy nadzieję, że dolecisz na miejsce.'''])
             time.sleep(2)
             os.system("cls")
             printing(['''Umarłeś_aś''',
-'''
-Niestety uszkodzenia statku były zbyt poważne
+'''Niestety uszkodzenia statku były zbyt poważne
 i przy starcie Twój statek kosmiczny stanął w płomieniach.
 Następnym razem wybieraj mądrze.'''])
             time.sleep(5)
             os.system("cls")
     else:
-        printing(['''
-Coś wydaje się być nie w porządku,
+        printing(['''Coś wydaje się być nie w porządku,
 jednakże nie za bardzo znasz się na budowie silnika statku kosmicznego.
 Czy chcesz poprosić o pomoc mechanika?'''])
         pygame.display.quit()
@@ -100,38 +89,32 @@ Czy chcesz poprosić o pomoc mechanika?'''])
         print("Nie - wciśnij 2")
         wybór_4 = int(input())
         if wybór_4 == 1:
-            printing(['''
-Mechanik sprawdził i naprawił twój statek.
+            printing(['''Mechanik sprawdził i naprawił twój statek.
 Wyniosło Cię to dużo monet, ale dzięki temu jesteś pewien/pewna,
 że dolecimy do każdej planety bezpiecznie.'''])
         else:
-            printing(['''
-Jakimś cudem samemu udało Ci się naprawić usterkę.
+            printing(['''Jakimś cudem samemu udało Ci się naprawić usterkę.
 Dzięki temu zaoszczędziłeś/zaoszczędziłaś trochę monet,
 ale nie wiadomo, czy usterka jest naprawiona poprawnie.
 Miejmy nadzieję, że dolecisz na miejsce.''',
-'''
-Jednak nadal coś wydaje się być nie tak z zbiornikiem paliwa,
+'''Jednak nadal coś wydaje się być nie tak z zbiornikiem paliwa,
 czy chcesz się temu przyjrzeć?'''])
         pygame.display.quit()
         print("Tak - wciśnij 1")
         print("Nie - wciśnij 2")
         wybór_5 = int(input())
         if wybór_5 == 1:
-            printing(['''
-Zbiornik paliwa wydaje się być uszkodzony.
+            printing(['''Zbiornik paliwa wydaje się być uszkodzony.
 Zdecyduj, która opcja naprawcza będzie najkorzystniejsza.'''])
             pygame.display.quit()
             print("Załatanie zbiornika - wciśnij 1")
             print("Wymiana całego zbiornika na nowy - wciśnij 2")
             wybór_2 = int(input())
             if wybór_2 == 1:
-                printing(['''
-Załatanie zbiornika było dobrym wyborem.
+                printing(['''Załatanie zbiornika było dobrym wyborem.
 Uszkodzenie nie było na tyle poważne, aby wymieniać cały zbiornik.'''])
             else:
-                printing(['''
-Wymiana całego zbiornika na nowy, była bezsensownym wydatkiem.
+                printing(['''Wymiana całego zbiornika na nowy, była bezsensownym wydatkiem.
 Załatanie zbiornika w zupełności by wystarczyło.
 No cóż, masz teraz mniej monet, ale przynajmniej nowy zbiornik.'''])
             printing(['''No to w drogę!'''])
@@ -141,8 +124,7 @@ No cóż, masz teraz mniej monet, ale przynajmniej nowy zbiornik.'''])
             time.sleep(2)
             os.system("cls")
             printing(['''Umarłeś_aś''',
-'''
-Niestety uszkodzenia statku były zbyt poważne
+'''Niestety uszkodzenia statku były zbyt poważne
 i przy starcie Twój statek kosmiczny stanął w płomieniach.
 Następnym razem wybieraj mądrze.'''])
             time.sleep(5)
